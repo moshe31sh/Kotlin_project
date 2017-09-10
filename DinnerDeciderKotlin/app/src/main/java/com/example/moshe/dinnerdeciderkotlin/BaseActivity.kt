e8a2794b8e939cbe7166ac8e68bad6739cccaffe
Moshe Shimon <moshe31sh@gmail.com>
2017-09-10 11:13:10 +0300

    first commit

---
+++ DinnerDeciderKotlin/app/src/main/java/com/example/moshe/dinnerdeciderkotlin/BaseActivity.kt
@@ -0,0 +1,16 @@
+package com.example.moshe.dinnerdeciderkotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+abstract class BaseActivity : AppCompatActivity() {
+
+    abstract fun getResourceLayout(): Int
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(getResourceLayout())
+    }
+
+
+}
